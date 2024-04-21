8f85d6f7e15643ab40133f28be91ac9b8229c3f3
Roman <ryzikov16@gmail.com>
2024-04-21 20:16:13 +0700

    add telebot

--- main.py
+++ main.py
@@ -6,19 +6,26 @@ bot=telebot.TeleBot(config.TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Привет, я бот, который поможет тебе узнать курс валюты.')
+    bot.send_message(message.chat.id, 'Напиши мне <имя валюты, цену которой он хочет узнать> <имя валюты, в которой надо узнать цену первой валюты> <количество первой валюты>')
+    bot.send_message(message.chat.id, 'Например: рубль доллар 100')
+    bot.send_message(message.chat.id, 'Доступны валюты: рубль, доллар, евро')
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, 'Напиши мне <имя валюты, цену которой он хочет узнать> <имя валюты, в которой надо узнать цену первой валюты> <количество первой валюты>')
+    bot.send_message(message.chat.id, 'Например: рубль доллар 100')
+    bot.send_message(message.chat.id, 'Доступны валюты: рубль, доллар, евро')
 
 @bot.message_handler(content_types=['text'])
 def dialog(message):
 
-    currency_conversion = CurrencyConverter('https://api.freecurrencyapi.com/v1/latest?apikey=' + config.API_KEY)
+    currency_conversion = CurrencyConverter('https://api.freecurrencyapi.com/v1/latest?apikey=' + config.API_KEY, message.chat.id)
     bot.send_message(message.chat.id, currency_conversion.currency_conversion(message.text))
 
-    
 
-try:
-    bot.polling(none_stop=True)
-except CustomException as e:
-    print(e) 
-
-# currency_conversion = CurrencyConverter('https://api.freecurrencyapi.com/v1/latest?apikey=' + config.API_KEY)
-# print(currency_conversion.currency_conversion('рубль доллар 100'))
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except CustomException as e:
+        bot.send_message(e.chat_id, e)
--- tools.py
+++ tools.py
@@ -1,7 +1,14 @@
 import requests
 
 class CustomException(Exception):
-    pass
+
+    def __init__(self, chat_id: int, message: str):
+        self.chat_id = chat_id
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
 
 class CurrencyConverter:
 
@@ -11,8 +18,9 @@ class CurrencyConverter:
         'евро': 'EUR',
     }
 
-    def __init__(self, url: str):
+    def __init__(self, url: str, chat_id: int):
         self.__url = url
+        self.__chat_id = chat_id
 
     @staticmethod
     def __get_exchange_rate(url: str, currencies: str, base_currency: str):
@@ -20,16 +28,17 @@ class CurrencyConverter:
         return data_json['data'][currencies]
 
     @staticmethod
-    def __processing_string(str: str, currency_list: dict):
+    def __processing_string(str: str, currency_list: dict, id: int):
         list_word = str.split(' ')
         if len(list_word) != 3:
-            raise CustomException('Неверная строка')
+            raise CustomException(id, 'Недопустимое количество параметров. Нужно ввести 3 параметра: имя валюты, имя валюты, количество. В единственном числе.')
+        
         # currencies / base_currency / count
         try:
             return currency_list[list_word[0]], currency_list[list_word[1]], int(list_word[2])
         except KeyError:
-            raise CustomException('Неверная валюта')
+            raise CustomException(id, 'Недопустимое имя валюты. Нужно ввести рубль, доллар или евро в единственном числе.')
 
     def currency_conversion(self, str: str):
-        currencies, base_currency, count = self.__processing_string(str, self.__сurrency_list)
+        currencies, base_currency, count = self.__processing_string(str, self.__сurrency_list, self.__chat_id)
         return self.__get_exchange_rate(self.__url, currencies, base_currency) * count
